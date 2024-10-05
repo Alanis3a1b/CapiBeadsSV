9767ce1381876169f10448a34f473a716ae32cf9
Alanis Alvarez <alanis.alvarez@catolica.edu.sv>
2024-10-04 22:43:32 -0600

    Se creo el controlador y vistas de el login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+//using capibeadsBD.Models; esto para cuando ya este lista la base de datos
+using System.Text.Json;
+
+namespace CapiBeadsSV.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
