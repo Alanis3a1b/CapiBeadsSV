a37961f3fbf6d80e71b97caa3f7843ca0d968481
waltershadow <jrescalanteaviles@gmail.com>
2024-11-08 23:16:54 -0600

    UsuariosCategorias, html y css

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,10 +23,18 @@ namespace CapiBeadsSV.Controllers
             return View();
         }
 
+        public IActionResult UsuarioCategoria()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+
+      
+    
 }
